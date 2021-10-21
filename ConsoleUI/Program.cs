6181ad034d58611e9fa39e62ba446689436f8e2f
pbansal16 <prateekbansalind@gmail.com>
2021-10-21 13:18:01 +1300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the user for a comma separated list of first names(no spaces).
+           
+            Console.Write("Write all the people first names separated by commas who are attending this party: ");
+            string firstName = Console.ReadLine();
+
+            //for example type  : prateek,john,harry
+
+            //Split the string into a string array.Loop through the array and tell each person hello.
+
+            string[] firstnames = firstName.Split(',');
+
+            for (int i = 0; i < firstnames.Length; i++)
+            {
+                Console.WriteLine($"Hello, {firstnames[i]}"); 
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
